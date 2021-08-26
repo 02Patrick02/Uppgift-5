2e2e732c1d95dd5b4958fbc5455fc12ee7143786
02Patrick02 <paku02001@utb.vaxjo.se>
2021-08-26 12:11:37 +0200

    added parseInt

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Globala variabler
 let inputElem;
 let msgElem;
+let fruitNames;
+let fruitNr;
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
 // Initiering av globala variabler samt koppling avfunktioner till knapparna.
@@ -18,13 +20,28 @@ function init() {
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 // Funktion som avgör vilken bild som ska visas
-function showFruit(a) {
+function showFruit() {
     let nr;
     let fruitUrl;
 
     nr = Number(inputElem[1].value);
     fruitUrl = document.getElementById("fruitImg");
     
+    //kollar om det är ett nummer eller inte
+    if(isNaN(nr)){
+        msgElem.innerHTML = "Skriv ett nummer";
+    }
+
+    //kollar om nummret som är inskrivet är mellan 1 och 5
+    if(nr < 1 || nr > 5){
+        msgElem.innerHTML = "Skriv ett nummer mellan 1 och 5";
+    }
+
+    //konverterar decimaltal till heltal
+    nr = parseInt(nr);
+    inputElem[1].value = nr;
+
+    //beroende på vilket nr det är så skrivs en annan bild ut
     if(nr == 1){
         fruitUrl.src = "./pics/fruit1.jpg";
     }
@@ -38,9 +55,7 @@ function showFruit(a) {
         fruitUrl.src = "./pics/fruit4.jpg";
     }
     else if(nr == 5) {
-        fruitUrl.src = "./pics/fruit5.jpg";
-    }   
-    if(isNaN(nr)){
-        msgElem.innerHTML = "Skriv ett nummer";
+        fruitUrl.src = "./pics/fruit5.jpg"; 
     }
+
 }
