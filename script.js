da30fb852b800163ab7cc6e417301e528c701514
02Patrick02 <paku02001@utb.vaxjo.se>
2021-08-31 13:12:21 +0200

    added addFruits

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let inputElem;
 let msgElem;
 let fruitNames;
 let fruitNr;
+let selFruitsElem;
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
 // Initiering av globala variabler samt koppling avfunktioner till knapparna.
@@ -14,13 +15,14 @@ function init() {
     inputElem[2]=document.getElementById("input2");
     inputElem[3]=document.getElementById("input3");
 
-    msgElem=document.getElementById("message");
+    msgElem = document.getElementById("message");
+    selFruitsElem = document.getElementById("selectedFruits")
     document.getElementById("btn1").onclick = showFruit;
     document.getElementById("btn2").onclick = checkName;
+    document.getElementById("btn3").onclick = addFruits;
 
     fruitNames = ["ingen frukt", "äpple", "banan", "citron", "apelsin", "päron"]
     fruitNr = 0;
-
 
 } // End init
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
@@ -94,6 +96,22 @@ function getNr(elemNr, high){
         return null;
     }
     return nr;
+}
+
+function addFruits(){
+    let amount = getNr(3,9);
+    let imgList;
+
+    imgList = "";
+    for(i = 0;i < amount;i++){
+        imgList += "<img src ='./pics/fruit" + fruitNr + ".jpg' alt = 'frukt'>";    
+    }
+
+    if(amount == null){
+        msgElem.innerHTML = "Ingen frukt är vald";
+        return;
+    }
+    selFruitsElem.innerHTML = imgList;
 }
 
 
